94867bfd15c0dcebfc53164a9d44e74d36b9786d
Artem Konovalov <izebit@gmail.com>
2023-12-14 20:30:10 +0100

    use pattern matching for records

--- word-count-2/app/Main.hs
+++ word-count-2/app/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 module Main where
 
 import qualified Data.Text.Encoding as TSE
@@ -9,27 +10,29 @@ data CounterType = BytesCounterType | SymbolsCounterType | WordsCounterType | Li
 data Counter = SimpleCounter { getType::CounterType, getResult::Int } | 
                WordsCounter { getType::CounterType, getResult::Int, getIntemediateResult::Int, isWord::Bool }
 
-handleSymbol :: Char -> [Counter] -> [Counter]
+handleSymbol:: Char -> [Counter] -> [Counter]
 handleSymbol symbol counters = map f counters where 
-    f counter = case (getType counter) of 
-        SymbolsCounterType -> counter { getResult = getResult counter + 1 }
-        BytesCounterType -> counter { getResult = getResult counter + increment } where
+    f counter = case counter of 
+        SimpleCounter { getType = SymbolsCounterType, getResult = resultValue } -> counter { getResult = resultValue + 1 }
+        SimpleCounter { getType = BytesCounterType, getResult = resultValue } -> counter { getResult = resultValue + increment } where
             text = singleton symbol
             byteString = TSE.encodeUtf8 text
             increment = B.length byteString 
-        WordsCounterType -> updatedCounter { getResult = getIntemediateResult updatedCounter + if isWord updatedCounter then 1 else 0 } where
-            isSpaceSymbol = isSpace symbol
-            increment = if isSpaceSymbol && isWord counter then 1 else 0
-            isWordValue = not isSpaceSymbol
-            updatedCounter = counter { isWord = isWordValue, getIntemediateResult = getIntemediateResult counter + increment}
-        LinesCounterType -> counter { getResult = getResult counter + increment } where
+        SimpleCounter {getType = LinesCounterType, getResult = resultValue } -> counter { getResult = resultValue + increment } where
             increment = if symbol == '\n' then 1 else 0
+        WordsCounter { getIntemediateResult = intermediateResultValue, isWord = isWordValue } -> updatedCounter where
+            isSpaceSymbol = isSpace symbol
+            updatedIntermediateResultValue = intermediateResultValue + if isSpaceSymbol && isWordValue then 1 else 0
+            updatedIsWordValue = not isSpaceSymbol
+            updatedResultValue = updatedIntermediateResultValue + if updatedIsWordValue then 1 else 0
+            updatedCounter = counter { isWord = updatedIsWordValue, getIntemediateResult = updatedIntermediateResultValue, getResult = updatedResultValue }
+        _ -> undefined
 
 createCounters :: [CounterType] -> [Counter]
 createCounters counterTypes = map f counterTypes where 
     f t = case t of 
         WordsCounterType -> WordsCounter { getType = t, getResult = 0, isWord = False, getIntemediateResult = 0 }
-        otherwise ->  SimpleCounter { getType = t, getResult = 0 }
+        _ ->  SimpleCounter { getType = t, getResult = 0 }
 
 handleString :: [Char] -> [CounterType] -> [(CounterType, Int)]
 handleString chars counterTypes = let
@@ -38,11 +41,20 @@ handleString chars counterTypes = let
     results = map (\c -> (getType c, getResult c)) handledCounters
     in results
 
+
+data InputType = Console | File deriving (Enum, Eq, Show)
+data InputParamereter = InputParamereter { inputType:: InputType, filePath::Maybe String, counterTypes::[CounterType]}
+getInputParameter :: String -> Either InputParamereter String 
+getInputParameter args = do
+
+    Right "error!"
+
 getOutputString :: [(CounterType, Int)] -> String
 getOutputString results = let 
     resultTokens = map (\pair -> (show $ fst pair) ++ " " ++ (show $ snd pair)) results
     output = foldr (\t s -> if s == "" then t else t ++ ", " ++ s) "" resultTokens
     in output
+
 
 
 main :: IO ()
